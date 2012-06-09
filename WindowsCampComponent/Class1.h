c193c08f6d6f1f9889ebfb4b5599e92bc8006718
Harry Pierson <harry@devhawk.net>
2012-06-09 16:02:24 -0700

    Updated Class1.h with SayHello method

--- WindowsCampComponent/Class1.h
+++ WindowsCampComponent/Class1.h
@@ -1,10 +1,18 @@
 ﻿#pragma once
 
+using namespace Platform;
+
 namespace WindowsCampComponent
 {
     public ref class Class1 sealed
     {
     public:
         Class1();
+
+        String^ SayHello(String^ name) {
+            return String::Concat(
+                ref new String(L"Hello there "),
+                name);
+        };
     };
 }
